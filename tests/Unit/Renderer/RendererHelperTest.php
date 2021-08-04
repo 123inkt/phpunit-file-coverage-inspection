67441118183fb4914ac2b1bb59e692db323d839e
Frank Dekker <fdekker@123inkt.nl>
2021-08-04 20:50:10 +0200

    Add coverage

---
+++ tests/Unit/Renderer/RendererHelperTest.php
@@ -0,0 +1,75 @@
+<?php
+declare(strict_types=1);
+
+namespace DigitalRevolution\CodeCoverageInspection\Tests\Unit\Renderer;
+
+use DigitalRevolution\CodeCoverageInspection\Model\Config\InspectionConfig;
+use DigitalRevolution\CodeCoverageInspection\Model\Metric\Failure;
+use DigitalRevolution\CodeCoverageInspection\Model\Metric\FileMetric;
+use DigitalRevolution\CodeCoverageInspection\Renderer\RendererHelper;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @coversDefaultClass \DigitalRevolution\CodeCoverageInspection\Renderer\RendererHelper
+ */
+class RendererHelperTest extends TestCase
+{
+    private InspectionConfig $config;
+
+    protected function setUp(): void
+    {
+        $this->config = new InspectionConfig('base-path', 80);
+    }
+
+    /**
+     * @covers ::renderReason
+     */
+    public function testRenderReasonGlobalCoverageTooLow(): void
+    {
+        $metric  = new FileMetric('foobar', 80, []);
+        $failure = new Failure($metric, 20, Failure::GLOBAL_COVERAGE_TOO_LOW, 5);
+
+        $message = RendererHelper::renderReason($this->config, $failure);
+        static::assertSame('Project per file coverage is configured at 20%. Current coverage is at 80%. Improve coverage for this class.', $message);
+    }
+
+    /**
+     * @covers ::renderReason
+     */
+    public function testRenderReasonCustomCoverageTooLow(): void
+    {
+        $metric  = new FileMetric('foobar', 70, []);
+        $failure = new Failure($metric, 30, Failure::CUSTOM_COVERAGE_TOO_LOW, 5);
+
+        $message = RendererHelper::renderReason($this->config, $failure);
+        static::assertSame('Custom file coverage is configured at 30%. Current coverage is at 70%. Improve coverage for this class.', $message);
+    }
+
+    /**
+     * @covers ::renderReason
+     */
+    public function testRenderReasonMissingMethodCoverage(): void
+    {
+        $metric  = new FileMetric('foobar', 70, []);
+        $failure = new Failure($metric, 30, Failure::MISSING_METHOD_COVERAGE, 5);
+
+        $message = RendererHelper::renderReason($this->config, $failure);
+        static::assertSame('File coverage is above 80%, but method has no coverage at all.', $message);
+    }
+
+    /**
+     * @covers ::renderReason
+     */
+    public function testRenderReasonUnnecessaryCustomCoverage(): void
+    {
+        $metric  = new FileMetric('foobar', 70, []);
+        $failure = new Failure($metric, 30, Failure::UNNECESSARY_CUSTOM_COVERAGE, 5);
+
+        $message = RendererHelper::renderReason($this->config, $failure);
+        static::assertSame(
+            'A custom file coverage is configured at 30%, but the current file coverage 70% ' .
+            'exceeds the project coverage 80%. Remove `foobar` from phpfci.xml custom-coverage rules.',
+            $message
+        );
+    }
+}
