19dec8f06376a3ac2ceefd73576eb65a92894879
Frank Dekker <fdekker@gmail.com>
2022-06-25 10:47:37 +0200

    Update FileInspectionConfig model to support directories.

--- tests/Unit/Lib/IO/InspectionConfigFactoryTest.php
+++ tests/Unit/Lib/IO/InspectionConfigFactoryTest.php
@@ -15,13 +15,14 @@ class InspectionConfigFactoryTest extends TestCase
 {
     /**
      * @covers ::fromDOMDocument
-     * @covers ::getConfiguration
+     * @covers ::getInspectionConfig
      */
     public function testFromDOMDocument(): void
     {
         $xml = '<?xml version="1.0" encoding="UTF-8"?>
             <phpfci min-coverage="85">
                 <custom-coverage>
+                    <directory path="dir/ectory" min="60"/>
                     <file path="a/b/c" min="80"/>
                 </custom-coverage>
             </phpfci>
@@ -35,6 +36,11 @@ class InspectionConfigFactoryTest extends TestCase
         static::assertSame(85, $config->getMinimumCoverage());
         static::assertFalse($config->isUncoveredAllowed());
 
+        $dir = $config->getPathInspection('dir/ectory/file');
+        static::assertNotNull($dir);
+        static::assertSame(60, $dir->getMinimumCoverage());
+        static::assertSame('dir/ectory', $dir->getPath());
+
         $file = $config->getPathInspection('a/b/c');
         static::assertNotNull($file);
         static::assertSame(80, $file->getMinimumCoverage());
@@ -43,7 +49,7 @@ class InspectionConfigFactoryTest extends TestCase
 
     /**
      * @covers ::fromDOMDocument
-     * @covers ::getConfiguration
+     * @covers ::getInspectionConfig
      */
     public function testFromDOMDocumentWithUncoveredMethodsAllowed(): void
     {
@@ -60,7 +66,7 @@ class InspectionConfigFactoryTest extends TestCase
 
     /**
      * @covers ::fromDOMDocument
-     * @covers ::getConfiguration
+     * @covers ::getInspectionConfig
      */
     public function testFromDOMDocumentWithUncoveredMethodsForcedDisallowed(): void
     {
