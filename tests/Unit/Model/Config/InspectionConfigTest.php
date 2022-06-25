8a956bab477674bf92c143f44351976a37f682db
Frank Dekker <fdekker@gmail.com>
2022-06-25 12:15:30 +0200

    Update FileInspectionConfig model to support directories.

--- tests/Unit/Model/Config/InspectionConfigTest.php
+++ tests/Unit/Model/Config/InspectionConfigTest.php
@@ -49,9 +49,45 @@ class InspectionConfigTest extends TestCase
         $config     = new InspectionConfig('/base\\path', 20, false);
         $config->addPathInspection($fileConfig);
 
-        static::assertNull($config->getPathInspection('invalid'));
         static::assertSame($fileConfig, $config->getPathInspection('foo/bar/file'));
         static::assertSame($fileConfig, $config->getPathInspection('/base/path/foo/bar/file'));
         static::assertSame($fileConfig, $config->getPathInspection('/base/path/foo/bar/path/to/file'));
+    }
+
+    /**
+     * @covers ::addPathInspection
+     * @covers ::getPathInspection
+     */
+    public function testGetPathInspectionWithFileAndDirectoryInspection(): void
+    {
+        $fileConfig      = new PathInspectionConfig(PathInspectionConfig::TYPE_FILE, 'foo/bar/file', 50);
+        $directoryConfig = new PathInspectionConfig(PathInspectionConfig::TYPE_DIR, 'foo/bar/', 60);
+        $config          = new InspectionConfig('/base\\path', 20, false);
+        $config->addPathInspection($fileConfig);
+        $config->addPathInspection($directoryConfig);
+
+        static::assertSame($directoryConfig, $config->getPathInspection('foo/bar/directory'));
+        static::assertSame($fileConfig, $config->getPathInspection('foo/bar/file'));
+        static::assertSame($directoryConfig, $config->getPathInspection('/base/path/foo/bar/directory'));
+        static::assertSame($fileConfig, $config->getPathInspection('/base/path/foo/bar/file'));
+    }
+
+    /**
+     * @covers ::addPathInspection
+     * @covers ::getPathInspection
+     */
+    public function testGetPathInspectionBestDirectoryInspection(): void
+    {
+        $directoryConfigA = new PathInspectionConfig(PathInspectionConfig::TYPE_DIR, 'foo/bar/', 60);
+        $directoryConfigB = new PathInspectionConfig(PathInspectionConfig::TYPE_DIR, 'foo/bar/directory', 50);
+        $config          = new InspectionConfig('/base\\path', 20, false);
+        $config->addPathInspection($directoryConfigA);
+        $config->addPathInspection($directoryConfigB);
+
+        static::assertNull($config->getPathInspection('foo/directory'));
+        static::assertSame($directoryConfigB, $config->getPathInspection('foo/bar/directory/file'));
+        static::assertSame($directoryConfigA, $config->getPathInspection('foo/bar/other/file'));
+        static::assertSame($directoryConfigB, $config->getPathInspection('/base/path/foo/bar/directory/file'));
+        static::assertSame($directoryConfigA, $config->getPathInspection('/base/path/foo/bar/other/file'));
     }
 }
